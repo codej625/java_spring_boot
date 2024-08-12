56f82cd6c000b16e1487a3884a61edbea1cba994
codej625 <codej625@gmail.com>
2024-08-12 09:48:02 +0900

    240812 update

--- spring_security/src/main/java/site/codej625/springsecurity/service/security/CustomAuthenticationSuccessHandler.java
+++ spring_security/src/main/java/site/codej625/springsecurity/service/security/CustomAuthenticationSuccessHandler.java
@@ -1,18 +1,3 @@
-package com.enicom.portal.service.security;
-
-import com.enicom.portal.domain.login.LoginBusinessLogic;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 @RequiredArgsConstructor
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
